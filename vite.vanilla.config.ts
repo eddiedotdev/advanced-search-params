28b356e4888662eaf24ed8b80cf952562539dbdb
Eddie <ehsanmarco@gmail.com>
2024-12-13 14:32:04 -0700

    organized final file structure

--- vite.vanilla.config.ts
+++ vite.vanilla.config.ts
@@ -1,23 +1,15 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import path from "path";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/vanilla/index.ts"),
+      entry: path.resolve(__dirname, "src/vanilla/index.ts"),
       name: "UseSearchParams",
       fileName: (format) => `use-search-params.${format}.min.js`,
       formats: ["iife"],
     },
     outDir: "cdn",
-    emptyOutDir: true,
     minify: true,
-    rollupOptions: {
-      output: {
-        globals: {
-          "use-search-params": "UseSearchParams",
-        },
-      },
-    },
   },
 });
